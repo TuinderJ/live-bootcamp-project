732d11e6f5dd973c9bbe5c95f4822ccf989c9bd4
tuinderj <joshua.tuinder@gmail.com>
2025-09-01 12:08:09 -0600

    added email type

---
+++ auth-service/src/domain/email.rs
@@ -0,0 +1,42 @@
+use validator::validate_email;
+
+#[derive(Debug, PartialEq)]
+pub struct Email(String);
+
+impl Email {
+    pub fn parse(address: String) -> Result<Self, String> {
+        if validate_email(&address) {
+            Ok(Self(address))
+        } else {
+            Err(format!("{} is not a valid email.", address))
+        }
+    }
+}
+
+impl AsRef<str> for Email {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn email_should_parse_valid_input() {
+        let email = Email::parse("asdf@asdf.com".to_string()).unwrap();
+        assert_eq!(email, Email("asdf@asdf.com".to_string()));
+    }
+
+    #[test]
+    fn email_should_error_with_invalid_input() {
+        let test_cases = ["@asdf.com", "asdf.com", "asdf@"];
+
+        for test_case in test_cases.iter() {
+            let result = Email::parse(test_case.to_string());
+
+            assert_eq!(result, Err(format!("{} is not a valid email.", test_case)));
+        }
+    }
+}
